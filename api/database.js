822bf43c41f335e2bb091f6b9c24f6e0beb67c94
YaKoRMuD <iamyakormud@gmail.com>
2025-05-13 23:29:53 +0700

    Update database.js

--- api/database.js
+++ api/database.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'myproject',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
-
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL database:', err);
+    console.error('❌ MySQL connection error:', err);
     return;
   }
-  console.log('Connected to MySQL database!');
+  console.log('✅ Connected to MySQL (Aiven)');
 });
 
 module.exports = connection;
