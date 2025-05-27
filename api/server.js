8df05360ec653e4d778cdd32889dc593734ab536
YaKoRMuD <iamyakormud@gmail.com>
2025-05-27 17:50:04 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -6,8 +6,10 @@ const port = 3000;
 const path = require('path');
 
 // Middleware
+//app.use(express.json());
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
+app.use(cors());
 
 //.env
 require('dotenv').config();
